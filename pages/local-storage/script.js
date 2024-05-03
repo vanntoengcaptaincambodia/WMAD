4e00eaf1f78a392cec997b3f4359b92a449351f1
siemhong <emsiemhong666@gmail.com>
2024-05-03 14:01:27 +0700

    First commit

---
+++ pages/local-storage/script.js
@@ -0,0 +1,15 @@
+function save() {
+  const email = document.getElementById("email").value;
+
+  // Save the email above email to local storage
+}
+
+function display() {
+  // ==change code only here== //
+  const email = ""; // get an email from local storage to
+  // ==change code only here== //
+
+
+  const display = document.getElementById("display-email");
+  display.innerHTML = email;
+}
---
+++ pages/url-param/script.js
@@ -0,0 +1,8 @@
+const theName = document.getElementById("name");
+const age = document.getElementById("age");
+const myClass = document.getElementById("class");
+
+
+theName.innerText = "aaa"; //  using URL parameter to get value and replace "aaa"
+age.innerText = 2222; //  using URL parameter to get value and replace 2222
+myClass.innerText = "cc"; //  using URL parameter to get value and replace "cc"
