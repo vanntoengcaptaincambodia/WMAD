1af7a4b342ec8639274f33172dfe5537c838aa98
siemhong <emsiemhong666@gmail.com>
2024-05-06 11:55:04 +0700

    Update exercise

--- pages/fetch-api/script.js
+++ pages/fetch-api/script.js
@@ -1,3 +1,15 @@
+
+function getPen() {
+
+    // Fetch pen colors from API
+    let pen = { id: 1, name: "Pen 1", color: "red" };
+    const penListDiv = document.getElementById("single-pen");
+
+    const penElement = createPenElement(pen);
+    penListDiv.appendChild(penElement);
+
+}
+
 function createPenElement(pen) {
   const penDiv = document.createElement("div");
   penDiv.classList.add("col-3", "text-center");
@@ -9,37 +21,11 @@ function createPenElement(pen) {
   const penNameP = document.createElement("p");
   penNameP.textContent = pen.name;
 
-  const penLink = document.createElement("a");
-  penLink.appendChild(penColorDiv);
-  penLink.appendChild(penNameP);
-
   penDiv.appendChild(penColorDiv);
   penDiv.appendChild(penNameP);
 
   return penDiv;
 }
 
-function getAllPens() {
-  // Fetch pens from API
-  //1. API : Replace pens data by fetching from API
-  let pens = [
-    { id: 1, name: "Pen 1", color: "red" },
-    { id: 2, name: "Pen 2", color: "blue" },
-    { id: 3, name: "Pen 3", color: "black" },
-    { id: 4, name: "Pen 4", color: "red" },
-    { id: 5, name: "Pen 5", color: "blue" },
-    { id: 6, name: "Pen 6", color: "black" },
-    { id: 7, name: "Pen 7", color: "red" },
-    { id: 8, name: "Pen 8", color: "blue" },
-    { id: 9, name: "Pen 9", color: "black" },
-  ];
-
-  const penListDiv = document.getElementById("pen-list");
-
-  pens.forEach((pen) => {
-    const penElement = createPenElement(pen);
-    penListDiv.appendChild(penElement);
-  });
-}
-
-getAllPens();
+// call function getPen
+getPen();
